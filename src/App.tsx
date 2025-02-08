b504bb46e7b230d4b5af09a40687ddd905d9f98a
Victoria Cheng <victoria.cheng15@gmail.com>
2025-02-07 22:32:44 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <header className='w-11/12 mx-auto p-6 text-center'>
+        <h1 className='text-3xl text-gray-100'>Hello, React and Jenkins</h1>
+      </header>
+    </>
+  )
+}
+
+export default App
